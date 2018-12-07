f39b0ad9679b484ed14173b7056132b1d4634801
Itsuki Goya <e185716@ie.u-ryukyu.ac.jp>
2018-12-07 17:13:55 +0900

    first commit

---
+++ src/main/java/jp/ac/uryukyu/ie/e185716/LivingThing.java
@@ -0,0 +1,68 @@
+package jp.ac.uryukyu.ie.e185716;
+
+public class LivingThing {
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
+
+    public LivingThing(String name, int hitpoint, int attack){
+        this.name = name;
+        this.hitPoint = hitpoint;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, hitpoint, attack);
+    }
+
+    public boolean isDead(){
+        return dead;
+    }
+
+
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getHitpoint(){
+        return hitPoint;
+    }
+
+    public void setHitpoint(int hitpoint) {
+        this.hitPoint = hitpoint;
+    }
+
+    public int getAttack(){
+        return attack;
+    }
+
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    public void attack(LivingThing livingThing){
+        int damage = (int) (Math.random() * attack);
+        if(dead == false ) {
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, livingThing.getName(), damage);
+            livingThing.wounded(damage);
+        }
+    }
+
+
+
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+
+}
